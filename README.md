0e66d2fdb40e8219648fa7e5c9dfa3264c2136fe
JJ Merelo <jjmerelo@gmail.com>
2016-11-05 19:15:07 +0100

    Changed lots of files
    You have added 36 and taken 2 lines

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # git-hooks
+
 A few Git hooks for the Perl Advent article
+
+Please remember that these hooks are not run in your shell, and they
+will use system-wide installed Perl. So you will need to install the
+corresponding modules, specially `File::Slurp::Tiny` if you want to
+run them from your git repos.
---
+++ prepare-commit-msg.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use File::Slurp::Tiny qw(read_file write_file);
+
+use v5.14;
+
+my $commit_msg_fn = shift || die "No commit message file";
+my $commit_msg = read_file( $commit_msg_fn );
+die if !$commit_msg;
+my $diff_output = `git diff-index HEAD --cached -p`;
+my @lines_changed = ($diff_output =~ /-\d+,(\d+) \+\d*,?(\d+)/gs);
+
+my ($lines_added, $lines_taken);
+
+while ( @lines_changed ) {
+  $lines_taken += shift @lines_changed;
+  $lines_added += shift @lines_changed;
+}
+
+my $message="\nYou have added $lines_added and taken $lines_taken lines";
+
+if ( $commit_msg !~ /$message/ ) {
+	write_file( $commit_msg_fn, $commit_msg.$message );
+}
