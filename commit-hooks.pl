581d06b809e9a5d53deae069908c689de481c8c2
JJ Merelo <jjmerelo@gmail.com>
2016-11-06 12:23:00 +0100

    And this is a new version
    That integrates all hooks
    
    You have added 40 and taken 15 lines

--- commit-hooks.pl
+++ commit-hooks.pl
@@ -9,15 +9,40 @@ use File::Slurp::Tiny qw(read_file write_file);
 
 use v5.14;
 
+sub diff_output {
+    my $git = shift;
+    $git->command('diff-index', '--cached','-p','HEAD');
+}
+
+PRE_COMMIT {
+    my ($git) = @_;
+
+    my @files_changed = (diff_output( $git) =~ /\++\sb\/(\S+)/gs);
+ 
+    my $syntax_ok = 0;
+    foreach my $file ( @files_changed ) {
+	next if ( $file !~ /\.p[ml]/ );
+	print "Checking $file  ";
+	my $output = `perl -cw $file 2>&1`;
+	if ($output =~ /syntax error/ ) {
+	    $syntax_ok = $syntax_ok || 1;
+	    say color("red"), "✗", color("reset"), "\n\tThere's an error in $file:", color("red"),
+	    join("",map( "\n\t$_", split("\n",$output))), color("reset");
+	} else {
+	    say color("green"),"✓", color("reset");
+	}
+    }
+    return $syntax_ok;
+};
+
+
 PREPARE_COMMIT_MSG {
 
     my ($git, $commit_msg_fn) = @_;
     my $commit_msg = read_file( $commit_msg_fn );
     die if !$commit_msg;
 
-    my $diff_output = $git->command(qw/diff-index --cached -p HEAD/);
-
-    my @lines_changed = ($diff_output =~ /-\d+,(\d+) \+\d*,?(\d+)/gs);
+    my @lines_changed = (diff_output( $git ) =~ /-\d+,(\d+) \+\d*,?(\d+)/gs);
 
     my ($lines_added, $lines_taken);
 
