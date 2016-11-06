8904325f9238142ad7b14ac6b538e0c6d0f9804c
JJ Merelo <jjmerelo@gmail.com>
2016-11-06 10:31:59 +0100

    Checks in syntax checker
    You have added 26 and taken 28 lines

--- pre-commit-msg.pl
+++ pre-commit-msg.pl
@@ -6,7 +6,6 @@ use warnings;
 use Git;
 
 use File::Slurp::Tiny qw(read_file write_file);
-use PPI;
 
 use v5.14;
 
@@ -14,21 +13,20 @@ my $repo = Git->repository();
 
 my $diff_output = $repo->command('diff-index', '--cached','-p','HEAD');
 
-say $diff_output;
 
-my @files_changed = ($diff_output =~ /\++\sb\/(\W+)/gs);
+my @files_changed = ($diff_output =~ /\++\sb\/(\S+)/gs);
  
-say @files_changed;
-
 my $syntax_ok = 0;
 foreach my $file ( @files_changed ) {
-    say "Checking $file\n";
-    my $file_content = read_file( $file );
-    my $document = PPI::Document->new( $file_content );
-    if ($document->errstr) {
-	$syntax_ok = $syntax_ok || 1;
-	say "There's an error in $file: \n\t", $document->errstr;
-    }
+  next if ( $file !~ /\.p[ml]/ );
+  print "Checking $file  ";
+  my $output = `perl -cw $file 2>&1`;
+  if ($output =~ /syntax error/ ) {
+    $syntax_ok = $syntax_ok || 1;
+    say "✗\n\tThere's an error in $file: \n\t", $output;
+  } else {
+    say "✓";
+  }
 }
 
 exit $syntax_ok;
